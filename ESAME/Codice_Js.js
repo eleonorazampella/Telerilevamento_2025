48cab69199cdd250b789e6d41acf81bdf0b729cf
eleonorazampella <eleonorazampella@gmail.com>
2025-09-16 15:48:05 +0200

    Update Codice_Js.js

--- ESAME/Codice_Js.js
+++ ESAME/Codice_Js.js
@@ -11,10 +11,12 @@
 // Nell'immagine è stata calcolata  la mediana 
 
 ``` JavaScript
+
 // ==============================================
 // Function to mask clouds using the QA60 band
 // Bits 10 and 11 correspond to opaque clouds and cirrus
 // ==============================================
+// Funzione per mascherare le nuvole Sentinel-2
 // Funzione per mascherare le nuvole Sentinel-2
 function maskS2clouds(image) {
   var qa = image.select('QA60');
@@ -25,48 +27,57 @@ function maskS2clouds(image) {
   return image.updateMask(mask).divide(10000);
 }
 
+
 // Area di interesse: Ourense (Galizia, Spagna)
-var geometry = ee.Geometry.Rectangle([-7.25, 42.25, -7.05, 42.40]);
-Map.centerObject(geometry, 12);
-Map.addLayer(geometry, {color: 'red'}, 'AOI Ourense');
+var aoi = ee.Geometry.Rectangle([-7.25, 42.25, -7.05, 42.40]);
+Map.centerObject(aoi, 12);
+Map.addLayer(aoi, {color: 'red'}, 'AOI Ourense');
+
 
 // Collezione Sentinel-2 pre-incendio
-var collection = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
-                   .filterBounds(geometry)
-                   .filterDate('2025-06-25', '2025-06-30')
-                   .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
-                   .map(maskS2clouds);
+var collection_pre = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+                        .filterBounds(aoi)
+                        .filterDate('2025-06-25', '2025-06-30')
+                        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
+                        .map(maskS2clouds);
 
-print('Numero immagini pre-incendio:', collection.size());
+print('Numero immagini pre-incendio:', collection_pre.size());
+
 
 // Composito mediano
-var composite = collection.median().clip(geometry);
+var composite_pre = collection_pre.median().clip(aoi);
+
 
 // Visualizzazioni
-Map.addLayer(composite, {bands: ['B4','B3','B2'], min:0, max:0.3},   // RGB naturale');
-Map.addLayer(composite, {bands: ['B8','B4','B3'], min:0, max:0.3},   // Falso colore');
-Map.addLayer(composite, {bands: ['B12','B8','B4'], min:0, max:0.3},  // SWIR-NIR-R');
-Map.addLayer(composite, {bands: ['B12'], min:0, max:0.3, palette:['black','white']}, // Banda SWIR');
+Map.addLayer(composite_pre, {bands: ['B4','B3','B2'], min:0, max:0.3}, 'RGB naturale PRE');
+Map.addLayer(composite_pre, {bands: ['B8','B4','B3'], min:0, max:0.3}, 'Falso colore PRE');
+Map.addLayer(composite_pre, {bands: ['B12','B8','B4'], min:0, max:0.3}, 'SWIR PRE');
 
-// Export con 5 bande
+
+// Export
 Export.image.toDrive({
-  image: composite.select(['B2','B3','B4','B8','B12']),
+  image: composite_pre.select(['B2','B3','B4','B8','B12']),
   description: 'Ourense_PreIncendio_Giugno2025',
   folder: 'GEE_exports',
   fileNamePrefix: 'PreIncendio_Giugno2025',
-  region: geometry,
+  region: aoi,
   scale: 10,
   crs: 'EPSG:4326',
   maxPixels: 1e13
 });
+
+``` 
 
 // Immagine Post incendio (dal 05/09/2025 al 10/09/2025)
 // Nell'immagine è stata calcolata  la mediana 
 
+``` JavaScript
+
 // ==============================================
 // Function to mask clouds using the QA60 band
 // Bits 10 and 11 correspond to opaque clouds and cirrus
 // ==============================================
+// Funzione per mascherare le nuvole Sentinel-2
 // Funzione per mascherare le nuvole Sentinel-2
 function maskS2clouds(image) {
   var qa = image.select('QA60');
@@ -77,44 +88,46 @@ function maskS2clouds(image) {
   return image.updateMask(mask).divide(10000);
 }
 
+
 // Area di interesse: Ourense (Galizia, Spagna)
-var geometry = ee.Geometry.Rectangle([-7.25, 42.25, -7.05, 42.40]);
-Map.centerObject(geometry, 12);
-Map.addLayer(geometry, {color: 'red'}, 'AOI Ourense');
+var aoi = ee.Geometry.Rectangle([-7.25, 42.25, -7.05, 42.40]);
+Map.centerObject(aoi, 12);
+Map.addLayer(aoi, {color: 'red'}, 'AOI Ourense');
+
 
 // Collezione Sentinel-2 post-incendio
-var collection = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
-                   .filterBounds(geometry)
-                   .filterDate('2025-09-05', '2025-09-10')
-                   .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
-                   .map(maskS2clouds);
+var collection_post = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+                        .filterBounds(aoi)
+                        .filterDate('2025-09-05', '2025-09-10')
+                        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
+                        .map(maskS2clouds);
 
-print('Numero immagini post-incendio:', collection.size());
+print('Numero immagini post-incendio:', collection_post.size());
+
 
 // Composito mediano
-var composite = collection.median().clip(geometry);
+var composite_post = collection_post.median().clip(aoi);
+
 
 // Visualizzazioni
-Map.addLayer(composite, {bands: ['B4','B3','B2'], min:0, max:0.3}, // RGB naturale');
-Map.addLayer(composite, {bands: ['B8','B4','B3'], min:0, max:0.3},  // Falso colore');
-Map.addLayer(composite, {bands: ['B12','B8','B4'], min:0, max:0.3},  // SWIR-NIR-R');
-Map.addLayer(composite, {bands: ['B12'], min:0, max:0.3, palette:['black','white']},  // Banda SWIR');
+Map.addLayer(composite_post, {bands: ['B4','B3','B2'], min:0, max:0.3}, 'RGB naturale POST');
+Map.addLayer(composite_post, {bands: ['B8','B4','B3'], min:0, max:0.3}, 'Falso colore POST');
+Map.addLayer(composite_post, {bands: ['B12','B8','B4'], min:0, max:0.3}, 'SWIR POST');
 
-// Export con 5 bande
+
+// Export
 Export.image.toDrive({
-  image: composite.select(['B2','B3','B4','B8','B12']),
+  image: composite_post.select(['B2','B3','B4','B8','B12']),
   description: 'Ourense_PostIncendio_Settembre2025',
   folder: 'GEE_exports',
   fileNamePrefix: 'PostIncendio_Settembre2025',
-  region: geometry,
+  region: aoi,
   scale: 10,
   crs: 'EPSG:4326',
   maxPixels: 1e13
 });
-
-                  
-
-
+               
+``` 
 
 // Entrambi i file sono state esportati su google drive e successivamente salvati sul computer e caricati in R
 
