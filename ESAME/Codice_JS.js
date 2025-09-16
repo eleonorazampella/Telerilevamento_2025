8b03d0cbdf3f4246ff29b4afdc802b4eeba1cc68
eleonorazampella <eleonorazampella@gmail.com>
2025-09-16 17:14:29 +0200

    Update Codice_JS.js

--- ESAME/Codice_JS.js
+++ ESAME/Codice_JS.js
@@ -3,11 +3,11 @@
 // Eleonora Zampella
 
 // CODICE IN JAVA SCRIPT PER SCARICARE LE IMMAGINI DI SENTINEL-2 DA GOOGLE EARTH ENGINE
-// Le immagini riguardano la zona di in Galizia,Spagna
+// Le immagini riguardano la zona di in Sierra della Culebra a Zamora,Spagna
 // Ho utilizzato il codice di Rocio Beatriz Cortes Lobos modificando le bande, l'area di interesse (indicata attraverso un poligono) e il periodo da me preso in considerazione. 
 // Le immagini prese sono quelle con una copertura nuvolosa inferiore al 20 % 
 
-// Immagine Pre incendio (dal 25/06/2025 al 30/06/2025) 
+// Immagine Pre incendio (dal 05/05/2022 al 10/05/2022) 
 // Nell'immagine è stata calcolata  la mediana 
 
 
@@ -17,120 +17,6 @@
 // Bits 10 and 11 correspond to opaque clouds and cirrus
 // ==============================================
 // Funzione per mascherare le nuvole Sentinel-2
-// Funzione per mascherare le nuvole Sentinel-2
-function maskS2clouds(image) {
-  var qa = image.select('QA60');
-  var cloudBitMask = 1 << 10;
-  var cirrusBitMask = 1 << 11;
-  var mask = qa.bitwiseAnd(cloudBitMask).eq(0)
-               .and(qa.bitwiseAnd(cirrusBitMask).eq(0));
-  return image.updateMask(mask).divide(10000);
-}
-
-
-// Area di interesse: Ourense (Galizia, Spagna)
-var aoi = ee.Geometry.Rectangle([-7.25, 42.25, -7.05, 42.40]);
-Map.centerObject(aoi, 12);
-Map.addLayer(aoi, {color: 'red'}, 'AOI Ourense');
-
-
-// Collezione Sentinel-2 pre-incendio
-var collection_pre = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
-                        .filterBounds(aoi)
-                        .filterDate('2025-06-25', '2025-06-30')
-                        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
-                        .map(maskS2clouds);
-
-print('Numero immagini pre-incendio:', collection_pre.size());
-
-
-// Composito mediano
-var composite_pre = collection_pre.median().clip(aoi);
-
-
-// Visualizzazioni
-Map.addLayer(composite_pre, {bands: ['B4','B3','B2'], min:0, max:0.3}, 'RGB naturale PRE');
-Map.addLayer(composite_pre, {bands: ['B8','B4','B3'], min:0, max:0.3}, 'Falso colore PRE');
-Map.addLayer(composite_pre, {bands: ['B12','B8','B4'], min:0, max:0.3}, 'SWIR PRE');
-
-
-// Export
-Export.image.toDrive({
-  image: composite_pre.select(['B2','B3','B4','B8','B12']),
-  description: 'Ourense_PreIncendio_Giugno2025',
-  folder: 'GEE_exports',
-  fileNamePrefix: 'PreIncendio_Giugno2025',
-  region: aoi,
-  scale: 10,
-  crs: 'EPSG:4326',
-  maxPixels: 1e13
-});
-
-
-
-// Immagine Post incendio (dal 05/09/2025 al 10/09/2025)
-// Nell'immagine è stata calcolata  la mediana 
-
-
-
-// ==============================================
-// Function to mask clouds using the QA60 band
-// Bits 10 and 11 correspond to opaque clouds and cirrus
-// ==============================================
-// Funzione per mascherare le nuvole Sentinel-2
-// Funzione per mascherare le nuvole Sentinel-2
-function maskS2clouds(image) {
-  var qa = image.select('QA60');
-  var cloudBitMask = 1 << 10;
-  var cirrusBitMask = 1 << 11;
-  var mask = qa.bitwiseAnd(cloudBitMask).eq(0)
-               .and(qa.bitwiseAnd(cirrusBitMask).eq(0));
-  return image.updateMask(mask).divide(10000);
-}
-
-
-// Area di interesse: Ourense (Galizia, Spagna)
-var aoi = ee.Geometry.Rectangle([-7.25, 42.25, -7.05, 42.40]);
-Map.centerObject(aoi, 12);
-Map.addLayer(aoi, {color: 'red'}, 'AOI Ourense');
-
-
-// Collezione Sentinel-2 post-incendio
-var collection_post = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
-                        .filterBounds(aoi)
-                        .filterDate('2025-09-05', '2025-09-10')
-                        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
-                        .map(maskS2clouds);
-
-print('Numero immagini post-incendio:', collection_post.size());
-
-
-// Composito mediano
-var composite_post = collection_post.median().clip(aoi);
-
-
-// Visualizzazioni
-Map.addLayer(composite_post, {bands: ['B4','B3','B2'], min:0, max:0.3}, 'RGB naturale POST');
-Map.addLayer(composite_post, {bands: ['B8','B4','B3'], min:0, max:0.3}, 'Falso colore POST');
-Map.addLayer(composite_post, {bands: ['B12','B8','B4'], min:0, max:0.3}, 'SWIR POST');
-
-
-// Export
-Export.image.toDrive({
-  image: composite_post.select(['B2','B3','B4','B8','B12']),
-  description: 'Ourense_PostIncendio_Settembre2025',
-  folder: 'GEE_exports',
-  fileNamePrefix: 'PostIncendio_Settembre2025',
-  region: aoi,
-  scale: 10,
-  crs: 'EPSG:4326',
-  maxPixels: 1e13
-});
-               
-// Entrambi i file sono state esportati su google drive e successivamente salvati sul computer e caricati in R
-
-Progetto sierra 
-
 // Funzione per mascherare le nuvole Sentinel-2
 function maskS2clouds(image) {
   var qa = image.select('QA60');
@@ -176,7 +62,17 @@ Export.image.toDrive({
 });
 
 
-post:
+
+// Immagine Post incendio (dal 20/08/2022 al 25/08/2022)
+// Nell'immagine è stata calcolata  la mediana 
+
+
+
+// ==============================================
+// Function to mask clouds using the QA60 band
+// Bits 10 and 11 correspond to opaque clouds and cirrus
+// ==============================================
+// Funzione per mascherare le nuvole Sentinel-2
 // Funzione per mascherare le nuvole Sentinel-2
 function maskS2clouds(image) {
   var qa = image.select('QA60');
@@ -220,3 +116,7 @@ Export.image.toDrive({
   crs: 'EPSG:4326',
   maxPixels: 1e13
 });
+               
+// Entrambi i file sono state esportati su google drive e successivamente salvati sul computer e caricati in R
+
+
