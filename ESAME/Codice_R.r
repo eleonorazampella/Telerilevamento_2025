4840bee0018d8d3a3c8da5a6d3c58304454c6213
eleonorazampella <eleonorazampella@gmail.com>
2025-09-16 17:54:46 +0200

    Update Codice_R.r

--- ESAME/Codice_R.r
+++ ESAME/Codice_R.r
@@ -1,10 +1,10 @@
-# PROGETTO ESAME - ANALISI DELL'IMPATTO DELL'INCENDIO IN GALIZIA IN PARTICOLARE A CHANDREXA DE QUEIXA
+# PROGETTO ESAME - ANALISI DELLA VEGETAZIONE PRE E POST- INCENDIO NELLA SIERRA DE LA CULEBRA ATTRAVERSO LE IMMAGINI SATELLITARI AVVENUTO NEL 2022 
 # Telerilevamento Geo-ecologico in R
 # Eleonora Zampella 
 
 # CODICE IN R PER L'ELABORAZIONE DELLE IMMAGINI 
 
-# Le immagini sono scaricate da Google Earth Engine attraverso il codice di Rocio Beatriz Cortes Lobos e sono relative all'incendio avventuto a Chandrexa de Queixa
+# Le immagini sono scaricate da Google Earth Engine attraverso il codice di Rocio Beatriz Cortes Lobos e sono relative all'incendio avventuto a Sierra de la Culebra
 
 # Pacchetti richiesti e utilizzati
 library(terra) # Pacchetto per l'analisi spaziale dei dati con vettori e dati raster
@@ -16,11 +16,14 @@ library(patchwork) # Pacchetto per comporre più grafici ggplot insieme
 # Imposto la working directory 
 setwd("~/Desktop/TELERILEVAMENTO_R")
 
-pre=rast("PreIncendio_Giugno2025.tif") # Importo la prima immagine
+pre=rast("PreIncendio_Maggio2022.tif") # Importo la prima immagine
 plot(pre) # Per visulaizzare l'immagine importata
 
-post=rast("PostIncendio_Settembre2025.tif") #Importo la seconda immagine
+post=rast("PostIncendio_Agosto2022.tif") #Importo la seconda immagine
 plot(post) # Per visulizzare la seconda immagine
 
 #Visualizzo le immagini in RGB
+im.plotRGB(pre, r = 1, g = 2, b = 3, title = "Pre-incendio")
+im.plotRGB(post, r = 1, g = 2, b = 3, title = "Post-incendio")
+
 
