bd4f29e3c72ad0750c9e78b1511a948e6e9a5635
eleonorazampella <eleonorazampella@gmail.com>
2025-09-17 10:36:32 +0200

    Update Codice_R.r

--- ESAME/Codice_R.r
+++ ESAME/Codice_R.r
@@ -16,25 +16,25 @@ library(patchwork) # Pacchetto per comporre più grafici ggplot insieme
 # Imposto la working directory 
 setwd("~/Desktop/TELERILEVAMENTO_R")
 
-pre=rast("PreIncendio_Maggio2022.tif") # Importo la prima immagine
+pre=rast("PreIncendio_Maggio2022.tif") # Importo la prima immagine e la nomino
 plot(pre) # Per visulaizzare l'immagine importata
 
-post=rast("PostIncendio_Agosto2022.tif") #Importo la seconda immagine
+post=rast("PostIncendio_Agosto2022.tif") #Importo la seconda immagine e la nomino 
 plot(post) # Per visulizzare la seconda immagine
 
 #Visualizzo le immagini in RGB
-im.multiframe(1,2)
-im.plotRGB(pre, r = 1, g = 2, b = 3, title = "Pre-incendio")
-im.plotRGB(post, r = 1, g = 2, b = 3, title = "Post-incendio")
-dev.off()
+im.multiframe(1,2) # Visualizzare un pannello grafico con 1 riga e 2 colonne 
+im.plotRGB(pre, r = 1, g = 2, b = 3, title = "Pre-incendio")  # Visualizzare l'immagine a veri colori 
+im.plotRGB(post, r = 1, g = 2, b = 3, title = "Post-incendio") # Visualizzare l'immagine a veri colori 
+dev.off() # Chiudere il pannello di Visualizzazione delle immagini
 
 # Visualizzo le quattro bande separate (RGB e NIR) per entrambe le immagini
-im.multiframe(1,2)
+im.multiframe(1,2) # Visualizzare un pannello grafico con 1 riga e 2 colonne
 plot(pre, main=c("B4-Red", "B3-Green", "B2-Blue", "B8-NIR"), col=magma(100))
 plot(post, main=c("B4-Red", "B3-Green", "B2-Blue", "B8-NIR"), col=magma(100))
 dev.off() # Chiudo il pannello grafico dopo aver salvato l'immagine in .png
 
-im.multiframe(2,4) #apro un pannello multiframe, ancora vuoto, di n°2 righe e n°4 colonne
+im.multiframe(2,4) # Visualizzare un pannello grafico con 2 righe e 4 colonne
 plot(pre[[1]], col = magma(100), main = "Pre - Red") #viene specificata la banda, il colore e il titolo
 plot(pre[[2]], col = magma(100), main = "Pre - Green")
 plot(pre[[3]], col = magma(100), main = "Pre - Blue")
@@ -44,7 +44,7 @@ plot(post[[1]], col = magma(100), main = "Post - Red")
 plot(post[[2]], col = magma(100), main = "Post - Green")
 plot(post[[3]], col = magma(100), main = "Post - Blue")
 plot(post[[4]], col = magma(100), main = "Post - NIR")
-dev.off()
+dev.off() # Chiudere il pannello di Visualizzazione delle immagini
 
 
 
